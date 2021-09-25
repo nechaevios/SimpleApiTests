d845a4ac589b7c488f66ebb98dce2431d138dbfc
Sergey <21218903+xchakiex@users.noreply.github.com>
2021-09-25 18:22:38 +0300

    Initial Commit

---
+++ SimpleApiTests/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SimpleApiTests
+//
+//  Created by Nechaev Sergey  on 25.09.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
